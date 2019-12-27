3bc4921300843842e2a92f54dafa1a4ea972d30e
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-12-26 20:07:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+WPF VS Theme Template
---
+++ WPF VS Theme Template/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace WPF_VS_Theme_Template
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuAboutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a empty template project for a WPF window with a theme mimicking VS2019.", "About");
+        }
+    }
+}
